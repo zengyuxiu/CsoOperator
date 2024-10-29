87eda376c7f3ac3074f4fa2b624a9c9955298425
zyx <zengyuxiu@nuaa.edu.cn>
2024-10-29 13:00:35 +0800

    clean

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -P ./image/k3s https://github.com/k3s-io/k3s/releases/download/v1.31.1%2Bk3s1/k3s
+wget -P ./image/k3s https://github.com/k3s-io/k3s/releases/download/v1.31.1%2Bk3s1/k3s-airgap-images-amd64.tar.gz
+
